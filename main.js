322efad86dd42e93c3b31507a16ce837722f77b0
Rafael Dos Santos <rafasennin@hotmail.com>
2023-03-11 10:17:17 -0300

    v1.6

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ const inputName = document.getElementById("input__name");
 const cardNumber = document.getElementById("cardHolder__number");
 const inputNumber = document.getElementById("input__number");
 
+const cardDate = document.getElementById("cardHolder__date");
+const inputDateMonth = document.getElementById("input__date__month");
+const inputDateYear = document.getElementById("input__date__year");
+
+const cardCvc = document.getElementById("cardHolder__cvc");
+const inputCvc = document.getElementById("input__cvc");
+
 inputName.addEventListener("keyup", ()=>{
     cardName.innerText = inputName.value;
 });
@@ -12,3 +19,20 @@ inputName.addEventListener("keyup", ()=>{
 inputNumber.addEventListener("keyup", ()=>{
     cardNumber.innerText = inputNumber.value;
 });
+
+inputDateMonth.addEventListener("keyup", ()=>{
+    cardDate.innerText = inputDateMonth.value;
+});
+
+inputDateYear.addEventListener("keyup", ()=>{
+    cardDate.innerText += inputDateYear.value;
+});
+
+inputCvc.addEventListener("keyup", ()=>{
+    cardCvc.innerText = inputCvc.value;
+});
+
+function confirmButton(){
+    window.location.reload();
+}
+
